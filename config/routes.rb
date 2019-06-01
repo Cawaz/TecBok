59b04e1ce312f41094af76e0becfb233f9e1f621
Cawaz <3cawaz3@gmail.com>
2019-06-01 17:18:13 +0900

    pry-byebugとannotateのインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: 'posts#index'
+  #
+  # resources :posts do
+  #   resources :comments, only: %i[create update destroy]
+  #   resources :likes, only: %i[create destroy]
+  # end
+
+  # devise_for :users
 end
