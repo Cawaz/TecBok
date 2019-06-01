905b17c60543436501436522076d69402937d295
Cawaz <3cawaz3@gmail.com>
2019-06-01 19:31:55 +0900

    annotate適用

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id          :bigint           not null, primary key
+#  rate        :integer          not null
+#  review      :text(65535)      not null
+#  title       :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :integer
+#  user_id     :integer
+#
+
 class Post < ApplicationRecord
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id          :bigint           not null, primary key
+#  rate        :integer          not null
+#  review      :text(65535)      not null
+#  title       :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :integer
+#  user_id     :integer
+#
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
