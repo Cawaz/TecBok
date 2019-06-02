832f50cafd5d2426d24ab83de902e92e9d3f1b96
Cawaz <3cawaz3@gmail.com>
2019-06-03 08:32:39 +0900

    投稿の登録機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,21 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save!
+      redirect_to root_path, notice: "登録しました。"
+    end
   end
 
   def edit
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :review, :rate)
   end
 end
