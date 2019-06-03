5f6bf53d84f1fb1d1f15f83b9bfc3e37d8b3879a
Cawaz <3cawaz3@gmail.com>
2019-06-03 13:22:07 +0900

    投稿の編集機能を修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -19,6 +20,12 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find( params[:id])
+    post.update(post_params)
+    redirect_to post, notice: "更新しました。"
   end
 
   def destroy
