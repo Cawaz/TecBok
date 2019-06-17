ccd414490fa11407a9c036ef36c914d268309736
Cawaz <3cawaz3@gmail.com>
2019-06-17 22:47:56 +0900

    usernameの表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,6 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @user = User.find(@post.user_id)
     @comment = Comment.new
     @comments = @post.comments
   end
