615c560687e78a3134c73803c39a2a31c2a98cc8
Cawaz <3cawaz3@gmail.com>
2019-06-17 20:42:49 +0900

    プロフィール画面の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    added_attrs = %i[username email password password_confirmation]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
