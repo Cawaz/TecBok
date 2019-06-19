db792700707e9ccf531b6da715a554cf4adf512e
Cawaz <3cawaz3@gmail.com>
2019-06-19 20:28:09 +0900

    ユーザのプロフィール画像編集機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :image
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
