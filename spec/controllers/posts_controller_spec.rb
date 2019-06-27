2a7f6e522085744b2a827616e0adf1fe4ab89c1d
Cawaz <3cawaz3@gmail.com>
2019-06-27 14:33:28 +0900

    Postのcreateメソッド正常系を追加

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  let(:user) { create(:user) }
+
+  before do
+    login_user user
+  end
+
   describe 'GET #new' do
     before { get :new }
 
@@ -15,5 +21,28 @@ RSpec.describe PostsController, type: :controller do
     it '新しいpostオブジェクトがビューに渡されること' do
       expect(assigns(:post)).to be_a_new Post
     end
+  end
+
+  describe 'POST #create' do
+    context '正しい投稿情報が渡って来た場合' do
+      let(:params) do
+        {post: {
+            title: '人を動かす',
+            review: '絶対に読むべき',
+            rate: 10,
+            user_id: 1
+          }
+        }
+      end
+
+      it '投稿が一つ増えていること' do
+        expect { post :create, params: params }.to change(Post, :count).by(1)
+      end
+
+      it '投稿一覧ページにリダイレクトされていること' do
+        expect(post :create, params: params).to redirect_to(root_path)
+      end
+    end
+
   end
 end
