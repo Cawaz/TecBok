63e0528535f3c7c1db8f35cb297d74c4d5eb9484
Cawaz <3cawaz3@gmail.com>
2019-07-07 09:58:49 +0900

    本番環境でBasic認証追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :authorize_basic
 
   def configure_permitted_parameters
     added_attrs = %i[username email avatar password password_confirmation]
@@ -26,6 +27,13 @@ class ApplicationController < ActionController::Base
       render json: { error: '404 error' }, status: :not_found
     else
       render 'errors/404', status: :not_found
+    end
+  end
+
+  def authorize_basic
+    return unless Rails.env.production?
+    authenticate_or_request_with_http_basic('BA') do |name, password|
+      name == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASS']
     end
   end
 end
